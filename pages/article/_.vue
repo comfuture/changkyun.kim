f4c9c72ef978beafd56d75d09ab27ac211d80844
comfuture <comfuture@gmail.com>
2020-07-01 09:29:07 +0900

    consider SEO on article pages

--- pages/article/_.vue
+++ pages/article/_.vue
@@ -10,6 +10,14 @@ export default {
   components: {
     ArticleView
   },
+  head() {
+    return {
+      title: `${this.article.title} - changkyun.kim`,
+      meta: [
+        {hid: 'description', name: 'description', content: this.article.description}
+      ]
+    }
+  },
   async asyncData({ $content, params: {pathMatch} }) {
     // static generate 되었을 때 canonical url 뒤에 trail slash 가 붙은 경우 컨텐츠를 찾을 수 없게 되는 것을 방지    
     const path = `article/${pathMatch.replace(/^\/|\/$/, '')}`
