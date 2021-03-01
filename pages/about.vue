fe21438287466cf87d2f142b5bd951bb8c8d03c9
comfuture <comfuture@gmail.com>
2021-03-02 00:37:27 +0900

    스타일링 시작

--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,113 @@
 <template>
-  <div class="container mx-auto items-center flex flex-wrap my-16 md:mt-64">
-    <div class="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
-      여기에 자기소개 넣을 예정
-    </div>
-  </div>
+  <main>
+    <section class="cover">
+      <img src="/image/cover.jpg" />
+    </section>
+    <section class="content">
+      <div class="container">
+        <div class="card">
+          <div class="w-full flex justify-center">
+            <div>
+              <img alt="..." src="/image/avatar.jpg" class="block shadow-xl rounded-full align-middle border-none absolute h-32 w-32 -m-16 md:h-48 md:w-48 md:-m-24">
+            </div>
+          </div>
+          <div class="bio">
+            <data class="name">김창균</data>
+            <i class="text-gray-500 material-icons">place</i> Seoul, KOREA <br/>
+            <i class="text-gray-500 material-icons">announcement</i> 한국어, 日本語, English <br/>
+          </div>
+          <div class="description prose md:prose-lg">
+            <h3>의로운 원칙주의자</h3>
+            <p>원리와 원칙을 중요시 여기며 말과 행동이 악의 편에 서지 않게 하기 위해 노력합니다.</p>
+            <h3>슬로우 러너</h3>
+            <p>빠르게 배우지 못하지만 꾸준히 익히며 깊이있게 이해하고 적절히 사용합니다.</p>
+            <h3>해결사</h3>
+            <p>데이터와 경험과 직관을 이용하여 문제를 해결하는 것을 좋아하고 잘 합니다.</p>
+          </div>
+          <div class="description">
+            <h3>관심있는 것</h3>
+            <ul>
+              <li>육아</li>
+              <li>요리</li>
+              <li>프로그래밍</li>
+              <li>캠핑</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 <script>
 export default {
   name: 'about'
 }
 </script>
+<style scoped lang="postcss">
+.material-icons {
+  vertical-align: middle;
+}
+
+main {
+  section.cover {
+    @md {
+      @apply pt-16;
+    }
+
+    img {
+      @apply object-cover w-full h-64;
+
+      @screen lg {
+        height: 300px !important;
+      }
+
+      @screen xl {
+        height: 400px !important;
+      }
+    }
+  }
+
+  section.content {
+    @screen md {
+      @apply bg-gray-200;
+    }
+
+    .container {
+      @apply mx-auto items-center flex flex-wrap;
+      
+      @screen md {
+        @apply -mt-16 pb-8
+      }
+
+      .card {
+        @apply flex flex-col w-full;
+        
+        @screen md {
+          @apply w-10/12 mx-auto rounded-lg bg-white shadow-lg;
+        }
+
+        @screen xl {
+          @apply w-8/12;
+        }
+
+        .bio {
+          line-height: 3rem;
+          @apply pt-24 px-4 text-center mb-4 border-b border-gray-300;
+
+          data.name {
+            @apply block text-xl;
+          }
+        }
+
+        .description {
+          @apply py-2 px-4;
+
+          h3 {
+            @apply mt-3;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
