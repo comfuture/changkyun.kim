d3c91ef56f2fde56b6b17f0ea391f04f4ac9d01d
comfuture <comfuture@iplateia.com>
2021-06-03 03:50:52 +0900

    sort articles by updated date

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
         <div class="p-2 md:p-4 w-full md:w-1/2" v-for="article in recent" :key="article.slug">
           <article class="max-w-4xl px-10 my-4 py-6 bg-white rounded-lg shadow-md">
             <div class="flex justify-between items-center">
-              <span class="font-light text-gray-600">{{ $moment(article.createdAt).format('LL') }}</span>
+              <span class="font-light text-gray-600">{{ $moment(article.updatedAt).format('LL') }}</span>
             </div>
             <div class="mt-2">
               <n-link :to="article.path" class="text-2xl text-gray-700 font-bold hover:text-gray-600" href="#">{{ article.title }}</n-link>
@@ -39,8 +39,8 @@ export default {
   async fetch() {
     this.page = await this.$content('index').fetch()
     this.recent = await this.$content('article', {deep: true})
-      .only(['title', 'description', 'path', 'createdAt'])
-      .sortBy('createdAt', 'desc')
+      .only(['title', 'description', 'path', 'updatedAt', 'createdAt'])
+      .sortBy('updatedAt', 'desc')
       .limit(5)
       .fetch()
   }
