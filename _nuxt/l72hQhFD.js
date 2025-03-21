7c751424228a44060015d7e29003bc80a059423b
comfuture <comfuture@users.noreply.github.com>
2025-03-21 04:31:57 +0000

    Deploy to GitHub pages

---
+++ _nuxt/l72hQhFD.js
@@ -0,0 +1,1 @@
+import{e as s,A as o,c as r,a,t,g as n,o as c}from"./C5C27nRP.js";const _=s({__name:"[tag]",setup(l){const{params:{tag:e}}=o();return(p,u)=>(c(),r("main",null,[a("h2",null,t(n(e))+"에 대한 글들",1),a("p",null,t(n(e))+" ... ",1)]))}});export{_ as default};
