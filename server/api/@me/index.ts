bb537758ce47f81994933f2e3a9e202a3d3a6e97
comfuture <comfuture@iplateia.com>
2025-04-03 16:03:36 +0900

    feat: add inbox and outbox endpoints with basic request handling

--- server/api/@me/index.ts
+++ server/api/@me/index.ts
@@ -7,12 +7,12 @@ export default defineEventHandler(async (event) => {
       'https://www.w3.org/ns/activitystreams',
       'https://w3id.org/security/v1',
     ],
-    id: 'https://changkyun.com/api/user/me',
+    id: 'https://changkyun.kim/api/@me',
     type: 'Person',
     preferredUsername: 'me',
-    inbox: 'https://changkyun.com/api/user/me/inbox',
-    outbox: 'https://changkyun.com/api/user/me/outbox',
-    followers: 'https://changkyun.com/api/user/me/followers',
-    following: 'https://changkyun.com/api/user/me/following',
+    inbox: 'https://changkyun.kim/api/@me/inbox',
+    outbox: 'https://changkyun.kim/api/@me/outbox',
+    followers: 'https://changkyun.kim/api/@me/followers',
+    following: 'https://changkyun.kim/api/@me/following',
   }
 })
