6dd7126d254bc1353d9820a8fca6b69d95d8e274
comfuture <comfuture@iplateia.com>
2025-04-03 14:56:27 +0900

    feat: add hard coded webfinger route for resource

---
+++ server/routes/.well-known/webfinger.ts
@@ -0,0 +1,32 @@
+import { defineEventHandler, getQuery, createError } from 'h3'
+export default defineEventHandler(async (event) => {
+  const { resource } = getQuery<{ resource: string }>(event)
+
+  if (!resource) {
+    return createError({
+      statusCode: 400,
+      statusMessage: 'Missing resource parameter',
+    })
+  }
+
+  if (resource !== 'acct:me@changkyun.kim') {
+    return createError({
+      statusCode: 404,
+      statusMessage: 'Resource not found',
+    })
+  }
+
+  const response = {
+    subject: resource,
+    aliases: ['https://changkyun.com/ap/user/me'],
+    links: [
+      {
+        rel: 'self',
+        type: 'application/activity+json',
+        href: event.node.req.url,
+      },
+    ],
+  }
+
+  return response
+})
