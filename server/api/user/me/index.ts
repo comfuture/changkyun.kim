12f77dab7fb16e117f92c58e7d6d881cdd0ac07b
comfuture <comfuture@iplateia.com>
2025-04-03 15:21:31 +0900

    fix: update response structure in user profile endpoint to return object instead of sending directly

--- server/api/user/me/index.ts
+++ server/api/user/me/index.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async (event) => {
   setResponseHeader(event, 'Content-Type', 'application/ld+json; profile="https://www.w3.org/ns/activitystreams"')
-  return send(event, {
+  return {
     '@context': [
       'https://www.w3.org/ns/activitystreams',
       'https://w3id.org/security/v1',
@@ -12,5 +12,5 @@ export default defineEventHandler(async (event) => {
     outbox: 'https://changkyun.com/ap/user/me/outbox',
     followers: 'https://changkyun.com/ap/user/me/followers',
     following: 'https://changkyun.com/ap/user/me/following',
-  })
+  }
 })
