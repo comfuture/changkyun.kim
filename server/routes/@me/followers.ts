3c03565fbcb5b88a8de061243cd90407431804b8
comfuture <comfuture@iplateia.com>
2025-04-11 13:25:51 +0900

    fix: update followers endpoint to return correct totalItems and items list

--- server/routes/@me/followers.ts
+++ server/routes/@me/followers.ts
@@ -1,15 +1,14 @@
 export default defineEventHandler(async (event) => {
   const db = useDatabase()
-  const { rows } = await db.sql`SELECT count(*) FROM activity WHERE actor_id = ${me.id} AND type = 'Follow'`
-  const totalItems = rows?.[0]?.count ?? 0 as number
-  const { rows: followers } = await db.sql`SELECT * FROM activity WHERE actor_id = ${me.id} AND type = 'Follow' LIMIT 10`
-  // const orderedItems = followers?.map((follower) => follower.object.id) ?? []
+  const { rows: followers } = await db.sql`SELECT * FROM activity WHERE actor_id = ${me.id} AND type = 'Follow'`
+  const items = followers?.map((follower) => follower.object as string) ?? []
+  const totalItems = items.length
   setJsonLdHeader(event)
   return {
     '@context': 'https://www.w3.org/ns/activitystreams',
     id: 'https://changkyun.kim/@me/followers',
-    type: 'OrderedCollection',
-    totalItems: 0, // Should be total count in DB
-    orderedItems: [], // Needs pagination in a real implementation
+    type: 'Collection',
+    totalItems,
+    items,
   }
 })
