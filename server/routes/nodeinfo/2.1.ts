b63fe0e0bf4ada2376a662b9dbb02b082f71839c
comfuture <comfuture@iplateia.com>
2026-05-08 14:50:23 +0900

    Address ActivityPub review feedback

--- server/routes/nodeinfo/2.1.ts
+++ server/routes/nodeinfo/2.1.ts
@@ -1,8 +1,8 @@
-import { getActivityPubNodeInfo } from "../../utils/fedify"
+import { getActivityPubNodeInfoJson } from "../../utils/fedify"
 
 export default defineEventHandler(async (event) => {
   setResponseHeader(event, "Access-Control-Allow-Origin", "*")
   setResponseHeader(event, "Content-Type", "application/json; profile=\"http://nodeinfo.diaspora.software/ns/schema/2.1#\"; charset=utf-8")
 
-  return await getActivityPubNodeInfo()
+  return await getActivityPubNodeInfoJson()
 })
